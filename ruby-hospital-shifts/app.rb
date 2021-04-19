4dec0b529a6622aec0ef72b0e9c2b13f96ba5b91
Alex Terenda <alexterenda@gmail.com>
2021-04-19 17:36:51 +0100

    Complete exercise

--- ruby-hospital-shifts/app.rb
+++ ruby-hospital-shifts/app.rb
@@ -3,6 +3,8 @@ require 'csv'
 
 # CLASSES
 
+# Doctor
+
 class Doctor
     attr_reader :rate_multiplier, :grade_hourly_rate, :employer_name
     
@@ -16,6 +18,8 @@ class Doctor
     end
 end
 
+# Department
+
 class Department
     attr_reader :rate_multiplier, :name
 
@@ -25,6 +29,8 @@ class Department
         @rate_multiplier = attributes[:rate_multiplier] || 1
     end
 end
+
+# Shift
 
 class Shift
     attr_accessor :id
@@ -55,9 +61,11 @@ class Shift
     end
 
     def set_total_payment
-        @doctor.grade_hourly_rate * @total_hours * @rate_multiplier
+        (@doctor.grade_hourly_rate * @total_hours * @rate_multiplier).round(2)
     end
 end
+
+# ShiftRepository
 
 class ShiftRepository
     def initialize
@@ -81,68 +89,163 @@ class ShiftRepository
     end
 end
 
-doctor = Doctor.new({
+# - - - - - - - - - - - - - - - - - - - -
+
+# INSTANTIATIONS
+
+# Departments
+
+general_medicine = Department.new({
     id: 1,
-    grade_title: 'GP',
-    grade_hourly_rate: 45,
-    employer_name: 'MWF Agency',
-    employer_type: 'agency',
-    rate_multiplier: 1.3,
+    name: 'General Medicine',
+    rate_multiplier: 1,
     })
-    
-department = Department.new({
-    id: 1,
+
+accident_and_emergency = Department.new({
+    id: 2,
     name: 'Accident and Emergency',
     rate_multiplier: 1.5,
     })
 
-shift = Shift.new({
+
+# Doctors
+
+mwf_hospital_doctor = Doctor.new({
     id: 1,
-    start_datetime: DateTime.new(2018,10,17,9),
-    end_datetime: DateTime.new(2018,10,17,15),
-    department: department,
-    doctor: doctor,
+    grade_title: 'GP',
+    grade_hourly_rate: 45,
+    employer_name: 'MWF Hospital',
+    employer_type: 'hospital',
+    rate_multiplier: 1,
     })
 
-shift2 = Shift.new({
+mwf_agency_doctor = Doctor.new({
+    id: 2,
+    grade_title: 'GP',
+    grade_hourly_rate: 45,
+    employer_name: 'MWF Agency',
+    employer_type: 'agency',
+    rate_multiplier: 1.8,
+    })
+
+tt_agency_doctor = Doctor.new({
+    id: 3,
+    grade_title: 'Surgeon',
+    grade_hourly_rate: 60,
+    employer_name: 'TT Agency',
+    employer_type: 'agency',
+    rate_multiplier: 1.3,
+    })
+
+tt_hospital_doctor = Doctor.new({
+    id: 4,
+    grade_title: 'Surgeon',
+    grade_hourly_rate: 60,
+    employer_name: 'TT Agency',
+    employer_type: 'hospital',
+    rate_multiplier: 1,
+    })
+
+# Shifts
+
+mon_1 = Shift.new({
     id: 1,
     start_datetime: DateTime.new(2018,10,17,9),
     end_datetime: DateTime.new(2018,10,17,15),
-    department: department,
-    doctor: doctor,
+    department: general_medicine,
+    doctor: mwf_hospital_doctor,
     })
-    
+
+mon_2 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,17,20),
+    end_datetime: DateTime.new(2018,10,18,8),
+    department: general_medicine,
+    doctor: mwf_agency_doctor,
+    })
+
+tue_1 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,18,9),
+    end_datetime: DateTime.new(2018,10,18,15),
+    department: accident_and_emergency,
+    doctor: tt_agency_doctor,
+    })
+
+tue_2 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,18,20),
+    end_datetime: DateTime.new(2018,10,19,8),
+    department: accident_and_emergency,
+    doctor: tt_hospital_doctor,
+    })
+
+wed_1 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,19,9),
+    end_datetime: DateTime.new(2018,10,19,15),
+    department: general_medicine,
+    doctor: mwf_hospital_doctor,
+    })
+
+wed_2 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,19,20),
+    end_datetime: DateTime.new(2018,10,20,8),
+    department: general_medicine,
+    doctor: mwf_agency_doctor,
+    })
+
+thu_1 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,20,9),
+    end_datetime: DateTime.new(2018,10,20,15),
+    department: accident_and_emergency,
+    doctor: tt_agency_doctor,
+    })
+
+thu_2 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,20,20),
+    end_datetime: DateTime.new(2018,10,21,8),
+    department: accident_and_emergency,
+    doctor: tt_hospital_doctor,
+    })
+
+fri_1 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,21,9),
+    end_datetime: DateTime.new(2018,10,21,15),
+    department: general_medicine,
+    doctor: mwf_hospital_doctor,
+    })
+
+fri_2 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,21,20),
+    end_datetime: DateTime.new(2018,10,22,8),
+    department: general_medicine,
+    doctor: mwf_agency_doctor,
+    })
+
+shifts_array = []
+shifts_array << mon_1 << mon_2 << tue_1 << tue_2 << wed_1 << wed_2 << thu_1 << thu_2 << fri_1 << fri_2
+
+# ShiftRepository
 
 shift_repository = ShiftRepository.new
-shift_repository.create(shift)
-shift_repository.create(shift2)
+
+shifts_array.each do |shift|
+    shift_repository.create(shift)
+end
+
+# Export shifts.csv
+
 shift_repository.export_csv
 
 
+# - - - - - - - - - - - - - - - - - - - -
 
-# p doctor
-# p '- - -'
-# p department
-# p '- - -'
-# p shift
-# p '- - -'
-# p shift_repository
-        
-        
 
-        
-
-# # INSTANTIATIONS
-
-# # SHIFTS
-
-# # All shifts start on 17/10/2018
-
-# # Shift 1
-# # Shift 1: 09:00 -> 15:00
-# # “General Medicine” department and has no extra multiplier
-
-# # Shift 2
-# # Shift 2: 20:00 -> 08:00
-# # “Accident and Emergency” department and pays the enhanced rate multiplier of 1.5
+# Comments
 
