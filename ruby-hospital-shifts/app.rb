79a3a4224a6a7eda6568e5503f76c84e878c3713
Alex Terenda <alexterenda@gmail.com>
2021-04-19 17:08:23 +0100

    Completed ShiftRepository class, added export_csv

--- ruby-hospital-shifts/app.rb
+++ ruby-hospital-shifts/app.rb
@@ -1,9 +1,10 @@
 require 'date'
+require 'csv'
 
 # CLASSES
 
 class Doctor
-    attr_reader :rate_multiplier, :grade_hourly_rate
+    attr_reader :rate_multiplier, :grade_hourly_rate, :employer_name
     
     def initialize(attributes = {})
         @id = attributes[:id]
@@ -16,7 +17,7 @@ class Doctor
 end
 
 class Department
-    attr_reader :rate_multiplier
+    attr_reader :rate_multiplier, :name
 
     def initialize(attributes = {})
         @id = attributes[:id]
@@ -26,6 +27,8 @@ class Department
 end
 
 class Shift
+    attr_accessor :id
+    attr_reader :start_datetime, :end_datetime, :total_hours, :rate_multiplier, :total_payment, :doctor, :department
 
     def initialize(attributes = {})
         @id = attributes[:id]
@@ -44,7 +47,7 @@ class Shift
     private
 
     def set_total_hours
-        (@end_datetime - @start_datetime) * 24
+        ((@end_datetime - @start_datetime) * 24).to_f
     end
 
     def set_rate_multiplier
@@ -53,6 +56,28 @@ class Shift
 
     def set_total_payment
         @doctor.grade_hourly_rate * @total_hours * @rate_multiplier
+    end
+end
+
+class ShiftRepository
+    def initialize
+        @shifts = []
+        @next_id = 1
+    end
+
+    def create(shift)
+        shift.id = @next_id
+        @shifts << shift
+        @next_id += 1
+    end
+
+    def export_csv
+        CSV.open('shifts.csv', 'wb') do |csv|
+            csv << ['shift_id', 'start_date', 'start_time', 'end_date', 'end_time', 'total_hours', 'rate_paid_at', 'total_payment', 'employer_name', 'department_name' ]
+            @shifts.each do |shift|
+                csv << [shift.id, shift.start_datetime.strftime("%d/%m/%Y"), shift.start_datetime.strftime("%H:%M"), shift.end_datetime.strftime("%d/%m/%Y"), shift.end_datetime.strftime("%H:%M"), shift.total_hours, shift.rate_multiplier.round(2), shift.total_payment, shift.doctor.employer_name, shift.department.name]
+            end
+        end
     end
 end
 
@@ -77,23 +102,31 @@ shift = Shift.new({
     end_datetime: DateTime.new(2018,10,17,15),
     department: department,
     doctor: doctor,
-
     })
 
-p doctor
-p '- - -'
-p department
-p '- - -'
-p shift
+shift2 = Shift.new({
+    id: 1,
+    start_datetime: DateTime.new(2018,10,17,9),
+    end_datetime: DateTime.new(2018,10,17,15),
+    department: department,
+    doctor: doctor,
+    })
     
-        
-        
-# class ShiftRepository
-#     # @shifts = []
 
-#     # Private save_csv method
-# end
+shift_repository = ShiftRepository.new
+shift_repository.create(shift)
+shift_repository.create(shift2)
+shift_repository.export_csv
 
+
+
+# p doctor
+# p '- - -'
+# p department
+# p '- - -'
+# p shift
+# p '- - -'
+# p shift_repository
         
         
 
